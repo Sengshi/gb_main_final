5bea297aadacee153cbbc0d039b1af0dd10ea9a7
Denis Kel <asket2013@ya.ru>
2024-02-20 09:18:55 +1000

    Create project, working code

---
+++ gb_main_final/gb_main_final/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FilterArrayByStringLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ввод исходного массива строк
+            Console.WriteLine("Введите исходный массив строк через запятую:");
+            string[] inputArray = Console.ReadLine().Split(",");
+
+            // Создание нового массива для отфильтрованных строк
+            string[] filteredArray = new string[inputArray.Length];
+
+            // Индекс для отфильтрованного массива
+            int filteredIndex = 0;
+
+            // Перебор исходного массива
+            foreach (string str in inputArray)
+            {
+                // Проверка длины строки
+                if (str.Length <= 3)
+                {
+                    // Добавление строки в отфильтрованный массив
+                    filteredArray[filteredIndex] = str;
+                    filteredIndex++;
+                }
+            }
+
+            // Удаление пустых элементов из отфильтрованного массива
+            Array.Resize(ref filteredArray, filteredIndex);
+
+            // Вывод отфильтрованного массива строк
+            Console.WriteLine("Отфильтрованный массив строк:");
+            string result = string.Join(",", filteredArray);
+            Console.WriteLine(result);
+        }
+
+     }
+}
